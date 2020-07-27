25981dc48132035d2f828f5d920ea898af89efee
Victor Adrianzen Lizama <vadrianzen@proveedor.intercorp.com.pe>
2020-07-26 23:33:19 -0500

    [feat] Add pivotal configuration files

--- app/config/cors.config.js
+++ app/config/cors.config.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 
 module.exports = app => {
     const corsOptions = {
-        origin: ["http://localhost:3000", "http://guan.pe"]
+        origin: ["http://localhost:3000", "https://guan.pe", "https://wwww.guan.pe"]
     };
     app.use(cors(corsOptions));
 };
